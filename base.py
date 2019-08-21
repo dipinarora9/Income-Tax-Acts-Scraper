98b35af720ab19cd720776fbc1ef68342e5bccee
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2019-08-21 22:24:37 +0530

    updated

--- base.py
+++ base.py
@@ -1,32 +1,119 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import pprint
+import keyboard
+import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Firefox(executable_path=r"F:\Projects\ML\geckodriver.exe")
 
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
-        self.labelResponse = QtWidgets.QLabel(Dialog)
-        self.labelResponse.setGeometry(QtCore.QRect(40, 70, 121, 20))
-        self.labelResponse.setObjectName("labelResponse")
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(50, 130, 311, 16))
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.lineEditName = QtWidgets.QLineEdit(Dialog)
-        self.lineEditName.setGeometry(QtCore.QRect(150, 70, 113, 20))
-        self.lineEditName.setObjectName("lineEditName")
-        self.ButtonClickMe = QtWidgets.QPushButton(Dialog)
-        self.ButtonClickMe.setGeometry(QtCore.QRect(150, 160, 75, 23))
-        self.ButtonClickMe.setObjectName("ButtonClickMe")
-
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.labelResponse.setText(_translate("Dialog", "Enter Your Name"))
-        self.label.setText(_translate("Dialog", ""))
-        self.ButtonClickMe.setText(_translate("Dialog", "Click"))
+driver.get("https://www.incometaxindia.gov.in/pages/acts/income-tax-act.aspx")
 
 
+radio = driver.find_element_by_class_name("noBold")
+
+chapter = radio.find_elements_by_tag_name("td")
+chapter[1].click()
+
+chapterIndex = 0
+
+sectionIndex = 0
+
+pageCount = 0
+
+data = dict()
+sec = dict()
+while True:
+    driver.switch_to.default_content()
+    radio = driver.find_element_by_class_name("noBold")
+    chapter = radio.find_elements_by_tag_name("td")
+    if len(chapter) == 0:
+        keyboard.press_and_release("shift + alt + c")
+        driver.switch_to.default_content()
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "search_result"))
+        )
+    except:
+        keyboard.press_and_release("shift + alt + c")
+        driver.switch_to.default_content()
+        print("Waiting")
+        time.sleep(2)
+    title = driver.find_elements_by_class_name("search_result")
+    if len(title) - 1 < chapterIndex:
+        nextPage = driver.find_elements_by_class_name("ms-paging")
+        nextPage[3].click()
+        pageCount += 1
+        if pageCount == 5:
+            break
+        chapterIndex = 0
+        sectionIndex = 0
+        continue
+
+    print(title[chapterIndex].text.split("\n")[0], "chapter     \n")
+    chapterTitle = title[chapterIndex].text.split("\n")[0]
+
+    title[chapterIndex].click()
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "search-result-inner"))
+        )
+    except:
+        keyboard.press_and_release("shift + alt + c")
+        driver.switch_to.default_content()
+        print("Waiting")
+        time.sleep(2)
+    results = driver.find_elements_by_class_name("search-result-inner")
+
+    if len(results) - 1 != chapterIndex:
+        time.sleep(2)
+        results = driver.find_elements_by_class_name("search-result-inner")
+    element = WebDriverWait(results[chapterIndex], 10).until(
+        EC.visibility_of_element_located((By.TAG_NAME, "li"))
+    )
+
+    sections = results[chapterIndex].find_elements_by_tag_name("li")
+
+    element = WebDriverWait(sections[sectionIndex], 10).until(
+        EC.element_to_be_clickable((By.TAG_NAME, "a"))
+    )
+    section = sections[sectionIndex].find_element_by_tag_name("a")
+
+    print(section.text, "section      \n")
+    sectionTitle = section.text
+    section.click()
+    sectionIndex += 1
+
+    frameList = driver.find_elements_by_tag_name("iframe")
+
+    driver.switch_to.frame(frameList[1])
+
+    element = WebDriverWait(driver, 10).until(
+        EC.invisibility_of_element_located((By.ID, "overlaybx_page"))
+    )
+
+    element = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "viewerContent"))
+    )
+
+    content = driver.find_element_by_class_name("viewerContent")
+
+    sec[sectionTitle] = {"description": content.text, "tags": []}
+
+    if sectionIndex == len(sections):
+        sectionIndex = 0
+        chapterIndex += 1
+        data[chapterTitle] = sec
+        sec = dict()
+
+    keyboard.press_and_release("shift + alt + c")
+
+
+final = json.dumps(data)
+file = open("new.json", "w")
+file.write(final)
+file.close()
